b6d37a1f544617f31d047bc7753f646c3a365dbc
adityajha <aj12102003@gmail.com>
2025-08-28 18:02:42 +0530

    Paralleziation done

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ const passport = require('passport');
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: process.env.CLIENT_URL,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
